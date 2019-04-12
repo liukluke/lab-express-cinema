6d8a578db81a205c5660696b80f08240b80a481c
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-04-12 15:29:59 +0200

    iteration 3 done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get("/movies", (req, res) => {
-  res.render('movies');
+router.post("/movies", (req, res) => {
+  Movie.find({})
+  .then((movies) => {
+    res.render('movies', {movies});
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
 });
 
 module.exports = router;
