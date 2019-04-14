e47f66aeca110374bb52ad51a1904ef709fd49be
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-04-14 19:44:22 +0200

    miss css

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -3,19 +3,25 @@ const router  = express.Router();
 const Movie = require('../models/Movie');
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+  .then((movies) => res.render('movies', {movies}))
+  .catch(() => next())
 });
 
-router.post("/movies", (req, res) => {
+router.post("/movies", (req, res, next) => {
   Movie.find({})
-  .then((movies) => {
-    res.render('movies', {movies});
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+  .then((movies) => res.render('movies', {movies}))
+  .catch(() => next())
+});
 
+router.get("/movies/:id", (req, res, next) => {
+  let moviesId = req.params.id;
+  Movie.findById(moviesId)
+  .then((movies) => res.render('info', movies))
+  .catch(() => next())
 });
 
 module.exports = router;
